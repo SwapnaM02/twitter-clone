6c28d56fbb304320279cefe0389f7e4ed8d5839d
SwapnaM02 <m.v.swapnareddy@gmail.com>
2025-02-05 09:26:16 +0530

    installed the tailwind css and desiyUI from the docs, made some changes in the tailwind.config.js file.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,7 @@ function App() {
   
   return (
     <>
-     <p className='text-5xl text-red-500 bg-blue-300'>Hello from app.jsx</p>
-     <button className="btn btn-primary" >Button</button>
-<button className="btn btn-neutral">Neutral</button>
-<button className="btn btn-primary">Primary</button>
-<button className="btn btn-secondary">Secondary</button>
-<button className="btn btn-accent">Accent</button>
-<button className="btn btn-ghost">Ghost</button>
-<button className="btn btn-link">Link</button>
+     
      
     </>
   )
