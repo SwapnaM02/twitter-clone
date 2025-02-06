5a62ecf673204c6bfe4b6d5c2d574972b36ab461
SwapnaM02 <m.v.swapnareddy@gmail.com>
2025-02-06 12:04:58 +0530

    3-Auth handlded and routes protected in client

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import {Route,Routes} from "react-router-dom";
+import {Route,Routes,Navigate} from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import LoginPage from "./pages/auth/login/LoginPage";
 import SignUpPage from "./pages/auth/signup/SignUpPage";
@@ -6,20 +6,66 @@ import Sidebar from "./components/common/Sidebar";
 import RightPanel from "./components/common/RightPanel";
 import NotificationPage from "./pages/notification/NotificationPage";
 import ProfilePage from "./pages/profile/ProfilePage";
+import {Toaster} from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
+import LoadingSpinner from "./components/common/LoadingSpinner";
+
 
 function App() {
+
+  //  get the details of the authenticated user.
+  // to able to fectch some data or to send some get request. we use the useQuery().
+  // this function will run by default.
+  const {data:authUser,isLoading,error,isError}=useQuery({
+    // we use queryKey to give a unique name to our query and refer to it later.
+    queryKey:['authUser'],
+    queryFn:async()=>{
+      try{
+        const res = await fetch("/api/auth/me");
+        const data= await res.json();
+
+        // if the user is unauthorized
+        if(data.error) return null;
+        if(!res.ok){
+          throw new Error(data.error || "Something went wrong");
+        }
+        console.log("authUser is here:",data);
+       return data;
+
+      }
+      catch(error){
+        throw new Error(error);
+
+      }
+    },
+    retry:false,
+
+
+  })
+
+  if(isLoading){
+    return (
+      <div className="flex justify-center items-center h-screen flex">
+        <LoadingSpinner size="lg" />
+      </div>
+    )
+  }
+
+  console.log("App.jsx file",authUser);
+
   
   return (
     <div className='flex max-w-6xl mx-auto'>
-      <Sidebar/>  {/* common component, caz it's not wrapped with routes */}
+      {authUser && <Sidebar/> } {/* common component, caz it's not wrapped with routes */}
       <Routes>
-				<Route path='/' element={<HomePage />} />
-				<Route path='/signup' element={<SignUpPage />} />
-				<Route path='/login' element={<LoginPage />} />
-        <Route path='/notifications' element={<NotificationPage/>} />
-        <Route path='/profile/:username' element={<ProfilePage/>} />
+				<Route path='/' element={ authUser?<HomePage />:<Navigate to="/login" />} />
+				<Route path='/signup' element={ !authUser?<SignUpPage />:<Navigate to='/' />} />
+				<Route path='/login' element={ !authUser?<LoginPage />:<Navigate to="/" />} />
+        <Route path='/notifications' element={ authUser?<NotificationPage/>:<Navigate to="/login" />} />
+        <Route path='/profile/:username' element={ authUser ?<ProfilePage/>:<Navigate to="/login"/>} />
 			</Routes>
-      <RightPanel/>
+      { authUser && <RightPanel/>}
+      <Toaster/>
     </div>
   )
 }
